2a49009ee7ccedffc68f933d4d384dc348cdc049
Aditya Chauhan <adchauhan1277@gmail.com>
2025-07-04 10:12:07 +0530

    first commit-third-time

--- java-script/script.js
+++ java-script/script.js
@@ -1,4 +1,4 @@
- AOS.init();
+AOS.init();
 
 // Header script
 
@@ -18,7 +18,7 @@
 
 // Auto-run count number
 
-$(document).ready(function() {
+$(document).ready(function () {
   $(".counter").counterUp();
 });
 
@@ -26,18 +26,36 @@ $(document).ready(function() {
 
 
 
-$('.pi-img').parallax({imageSrc: './images/behavior-health.jpg'});
-
-// animation on scroll
-
-
-// slider
-
-$('#slider').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-//   asNavFor: '.slider-for',
-  dots: true,
-  centerMode: true,
-  focusOnSelect: true
+$(document).ready(function () {
+  $('#slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 });
